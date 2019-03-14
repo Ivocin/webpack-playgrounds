2f98fb767770f6347c1e44cda9c5fab9dd7c9658
Ivocin <1044514593@qq.com>
2019-03-14 16:48:13 +0800

    Output Management - Preparation

--- guides/src/index.js
+++ guides/src/index.js
@@ -1,19 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.svg';
 import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   let element = document.createElement('div');
+  let btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-  
-  // Add the image to our existing div.
-  var myIcon = new Image();
-  myIcon.src = Icon;
 
-  element.appendChild(myIcon);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   console.log(Data);
 
