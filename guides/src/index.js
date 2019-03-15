c4c1ca4160b31e489e8d6f736d227a4e7c231a7a
Ivocin <1044514593@qq.com>
2019-03-15 15:30:09 +0800

    Using webpack-dev-server
    理解 contentBase 作用

--- guides/src/index.js
+++ guides/src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 import Data from './data.xml';
 import printMe from './print.js';
+import './style.css'
 
 function component() {
   let element = document.createElement('div');
   let btn = document.createElement('button');
+
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
@@ -13,6 +15,10 @@ function component() {
   btn.onclick = printMe;
   element.appendChild(btn);
 
+  
+  var myIcon = new Image();
+  myIcon.src = './test.svg';
+  element.appendChild(myIcon);
   console.log(Data);
 
   return element;
