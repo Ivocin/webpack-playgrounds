6b0ea35345c6ffe32e6c0c4d7dbc3d39dd306693
Ivocin <1044514593@qq.com>
2019-03-18 15:41:11 +0800

    add antd, css loader

--- react-boilerplate/src/index.js
+++ react-boilerplate/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-
+import Button from 'antd/lib/button';
+import "antd/dist/antd.css";
+import "./App.css"
 const App = (props) => {
   return (
-    <h1>{props.data} React Boilerplate</h1>
+    <>
+      <h1>{props.data} React Boilerplate</h1>
+      <Button type="primary">Button</Button>
+    </>
   )
 }
 
